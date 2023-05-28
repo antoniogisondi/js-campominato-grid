571f8cf3e6fed813049140ab6d31f7b101fe24da
antoniogisondi <anto.gisondi@tiscali.it>
2023-05-28 18:47:30 +0200

    Exercise done with bonus

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
 "use strict"
+    
+// CREO LA FUNZIONE CHE GENERA LA GRIGLIA
+    function grid(caselle){
+        let container = document.createElement("div")
+        container.setAttribute('class', 'container mt-5')
+        document.body.appendChild(container);
+        let table = document.createElement("table")
+        container.appendChild(table)
+        let cnt = 1;
+        for(let i = 0; i<caselle; i++){
+            let row = document.createElement("tr")
+            table.appendChild(row)
+            for(let j = 0; cnt<=10, j<caselle; j++){
+                let column = document.createElement("td")
+                row.appendChild(column)
+                let columnText = document.createTextNode(`${cnt}`)
+                let div = document.createElement("div")
+                column.appendChild(div)
+                div.appendChild(columnText)
+                div.setAttribute('class', 'column ')
+                div.addEventListener('click', function(){
+                    this.classList.toggle("column")
+                    console.log(this)
+                })
+                cnt++  
+            }
+        }
+    }
+
+// CREO L'EVENTO CHE MI GENERA LA GRIGLIA IN BASE AL LIVELLO DI DIFFICOLTA' 
+let button = document.querySelector(".play")
+button.addEventListener('click', function(){
+   let select = document.querySelector(".form-select").value
+   if(select == 3){
+    grid(7)
+   }
+   else if(select == 2){
+    grid(9)
+   }
+   else{
+    grid(10)
+   }
+})
+
+let reset = document.querySelector(".reset")
+reset.addEventListener('click', function(){
+})
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 PROBLEMA: L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100. Ci saranno quindi 10 caselle per ognuna delle 10 righe. Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+SOLUZIONE:
+1 Creare la funzione che genera la griglia di gioco;
+2 Collegare la griglia di gioco ad un evento che la creerà in base al livello di difficoltà scelto;
+3 Al click del tasto play questa griglia viene creata al giocatore a schermo 
+4 Creare un evento che faccia cambiare colore alle caselle al click
